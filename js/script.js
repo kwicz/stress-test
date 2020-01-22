4e9a125ded75089f6f83c770986180d338bc0352
Student <example@example.com>
2020-01-22 13:09:38 -0800

    add more logic into the app

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
   $("form#stress_survey").submit(function(event){
     event.preventDefault();
-    $("input:checkbox[name=stressor]:checked").each(function(){
-      var negFactors = $(this).val();
-      $('#stressed').removeClass("hidden");
-      $('#stressed').append(negFactors + "<br>");
-    });
+    var check = $("input:checkbox[name=stressor]:checked").val();
+    if (check) {
+      $("input:checkbox[name=stressor]:checked").each(function(){
+        var negFactors = $(this).val();
+        $('#stressed').show();
+        $('#stressed').append(negFactors + "<br>");
+      });
+    } else {
+      $('#not-stressed').show();
+    }
     $('#stress_survey').hide();
   });
 });
